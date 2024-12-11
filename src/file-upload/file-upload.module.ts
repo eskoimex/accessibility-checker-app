016030a772e84580f02e5948b9602e73b245e5ed
unknown <samuel.imex@gmail.com>
2024-12-11 15:46:12 +0100

    added the new analysis to the file module

--- src/file-upload/file-upload.module.ts
+++ src/file-upload/file-upload.module.ts
@@ -3,10 +3,12 @@ import { FileUploadController } from './file-upload.controller';
 import { FileUploadService } from './file-upload.service';
 import { HeadingAnalysisService } from '../helpers/heading-analysis.helpers';
 import { ImageAnalysisService } from '../helpers/image-analysis.helpers';
+import { UniqueIdAnalysisService } from '../helpers/unique-id-analysis';
+import { FormLabelAnalysisService } from '../helpers/form-label-analysis';
 
 @Module({
   controllers: [FileUploadController],
-  providers: [FileUploadService, HeadingAnalysisService, ImageAnalysisService],
+  providers: [FileUploadService, HeadingAnalysisService, ImageAnalysisService, UniqueIdAnalysisService, FormLabelAnalysisService],
 
 })
 export class FileUploadModule {}
